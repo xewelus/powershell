667bfb747bc2aad7a439b5ae2fe30d3b54a73f36
Dinar Shakirov <dinar@esterdev.com>
2021-04-15 18:24:34 +0300

    io.ps1: SortAsFileSystem

--- core/io.ps1
+++ core/io.ps1
@@ -23,3 +23,46 @@ function CollectFiles {
 
     return $files;
 }
+
+function SortAsFileSystem
+{
+    param (
+        [List[string]] $files
+    )
+    
+    Add-Type -TypeDefinition @'
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+namespace NaturalSort {
+    public static class NaturalSort
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        public static extern int StrCmpLogicalW(string psz1, string psz2);
+    }
+    public class NaturalStringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return NaturalSort.StrCmpLogicalW(x.ToString(), y.ToString());
+        }
+    }
+}
+'@
+
+    [IComparer[string]]$comparer = New-Object NaturalSort.NaturalStringComparer
+    $files.Sort($comparer)
+}
+
+function TestSortAsFileSystem
+{
+    [List[string]] $files =  [List[string]]::new()
+    $files.Add("xxx")
+    $files.Add("_zzz")
+
+    SortAsFileSystem $files
+    $files
+}
+
+#TestSortAsFileSystem
